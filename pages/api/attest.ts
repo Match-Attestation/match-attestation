86060d3f8b291f9ed98b10d86d55d7e4e7a4d642
Alik Vovkotrub <alikvovk@icloud.com>
2024-02-10 15:50:16 +0200

    implement most of butotn

--- pages/api/attest.ts
+++ pages/api/attest.ts
@@ -1,4 +1,4 @@
-import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit';
+import { FrameRequest, getFrameHtmlResponse, getFrameMessage } from '@coinbase/onchainkit';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Match, MATCH_EXPIRY } from "@/app/types";
 import { kv } from "@vercel/kv";
@@ -6,6 +6,7 @@ import { ethers } from "ethers";
 import { EAS, Offchain, SchemaEncoder, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
 
 import { NextRequest, NextResponse } from 'next/server';
+import { SignerOrProvider } from '@ethereum-attestation-service/eas-sdk/dist/transaction';
 
 async function getResponse(req: NextRequest) {
     let matchId = req.nextUrl.searchParams.get('id');
@@ -28,7 +29,19 @@ async function getResponse(req: NextRequest) {
         return NextResponse.json({ error: 'Invalid message' }, { status: 400 });
     }
 
-    const { input, fid } = message;
+    const { input, interactor, liked, recasted } = message;
+
+    if (!input) {
+        return NextResponse.json({ error: 'Missing input' }, { status: 400 });
+    }
+
+    if (!liked || !recasted) {
+        return new NextResponse(
+            getFrameHtmlResponse({
+                image: '/images/you_must_like_and_recast.png',
+            })
+        )
+    }
 
     let winners = input.trim().split(',').map((i) => parseInt(i));
     if (winners.length < 1) {
@@ -52,11 +65,46 @@ async function getResponse(req: NextRequest) {
         "sepolia"
     );
 
-    // Connects an ethers style provider/signingProvider to perform read/write functions.
-    // MUST be a signer to do write operations!
-    eas.connect(provider);
+    const privateKey = process.env.PRIVATE_KEY;
+    if (!privateKey) {
+        throw new Error('Private key is missing');
+    }
 
-    return NextResponse.json({ success: true });
+    const signer: ethers.Signer = new ethers.Wallet(privateKey, provider);
+
+    eas.connect(signer as any);
+
+    // Initialize SchemaEncoder with the schema string
+    const schemaEncoder = new SchemaEncoder("bytes32 id,string title,bytes32 referee,bytes32[] players,bytes32[] winnners");
+    const encodedData = schemaEncoder.encodeData([
+        { name: "id", value: interactor.fid, type: "bytes32" },
+        { name: "title", value: match.title, type: "string" },
+        { name: "referee", value: match.referee, type: "bytes32" },
+        { name: "players", value: match.users, type: "bytes32[]" },
+        { name: "winnners", value: match.winner, type: "bytes32[]" }
+    ]);
+    const schemaUID = "0xf0146b763d066465cb2fa39a158ff83843875282abb607cb053c11cadc60fc4a";
+
+    const tx = await eas.attest({
+        schema: schemaUID,
+        data: {
+            recipient: "0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165",
+            expirationTime: BigInt(Date.now() + 100 * 60 * 60 * 24 * 365),
+            revocable: true,
+            data: encodedData,
+        },
+    });
+
+    const newAttestationUID = await tx.wait();
+
+    console.log("New attestation UID:", newAttestationUID);
+
+    return new NextResponse(
+        getFrameHtmlResponse({
+            // FIXME: actual image url
+            image: '/images/attested.png',
+        })
+    )
 }
 
 export async function POST(req: NextRequest) {
